d6eaab8fa97540c8971f50512d2f6ee5c4886645
Kevin Beick <kbeick@zoho.com>
2014-04-22 00:18:47 -0700

    basic read and execution working

--- shell.c
+++ shell.c
@@ -5,6 +5,10 @@
 #include <stdio.h>
 
 #define MAXINPUT (1025)
+#define NUM_ARGS (50)
+#define MAX_LEN_ARG (500)
+
+extern char** environ;
 
 
 
@@ -12,51 +16,57 @@ int main()
 {
     char prompt[10] = "kevin# ";
     char readin_err_msg[85] = "The shell doesn't like your input. Make sure your input is less than 1024 bytes.\n";
-    char input[MAXINPUT];
-    int nbyts_read;
+    char* usrCmd[NUM_ARGS];
+    char tmp[MAX_LEN_ARG];
+    char c[1];
+    int inputCharCount, cmdIndex, arglen;
+
 
     while(1){
+    PROMPT_USER:
         write(STDOUT_FILENO, prompt, sizeof(prompt));
-        input[0] = '\0'; // "Clear" the string
-        input[MAXINPUT-1] = '\0'; // "Clear" the string
+        inputCharCount = 0;
+        cmdIndex = 0;
+        arglen = 0;
 
         // Read user's input from standard in
-        nbyts_read = read(STDIN_FILENO, input, MAXINPUT);
-        // printf("the number of bytes read: %d", nbyts_read);
-        fflush(stdout);
-        if (nbyts_read < 0){
-            write(STDOUT_FILENO, readin_err_msg, sizeof(readin_err_msg));
-            continue;
-        }
-        else if(input[ MAXINPUT - 1 ] != '\n' && input[ MAXINPUT - 1 ] != '\0') {
-            write(STDOUT_FILENO, readin_err_msg, sizeof(readin_err_msg));
-            continue;
-        }
-
-        // Tokenize the user's input
-        char* usrCmd[50];
-        int cmdIndex, newTokInd, i = 0;
-        for(i=0; i<MAXINPUT; i++){
-            if (input[i] == ' ' || input[i] == '\t' || input[i] == '\n'){
-                usrCmd[cmdIndex] = &input[newTokInd];
-                // usrCmd[cmdIndex][i - newTokInd] = "\0";
-                newTokInd = i+1;
+        while(read(STDIN_FILENO, c, 1) > 0){
+            if (inputCharCount > MAXINPUT){
+                write(STDOUT_FILENO, readin_err_msg, sizeof(readin_err_msg));
+                goto PROMPT_USER;
+            }
+            if( c[0] == '\n'){
+            // End of command
+                tmp[arglen] = '\0';
+                usrCmd[cmdIndex] = tmp;
+                usrCmd[cmdIndex+1] = NULL;
+                break;
+            }else if (c[0] == ' ' || c[0] == '\t'){
+            // tokenize command
+                tmp[arglen] = '\0';
+                usrCmd[cmdIndex] = tmp;
                 cmdIndex++;
-            }else if (input[i] == '\0'){break;}
+                arglen = 0;
+            }else{
+            // normal character
+                tmp[arglen] = c[0]; 
+                arglen++;
+            }
+            inputCharCount++;
         }
-        usrCmd[cmdIndex] = NULL;
 
+        int i;
         for(i=0; i<=cmdIndex; i++){
             printf("%s\n", usrCmd[i]);
         }fflush(stdout);
         
-        // // Fork a new process
-        // pid = fork();
-        // if (pid < 0){continue;} //TODO provide some error msg here TODO
-        // if (!pid){//Child
-        //     execve(cmd, ...);
-        // }else{//Parent
-        //     wait();
-        // }
+        // Fork a new process
+        int pid = fork();
+        if (pid < 0){continue;} //TODO provide some error msg here TODO
+        if (!pid){//Child
+            execve(usrCmd[0], usrCmd, environ);
+        }else{//Parent
+            wait();
+        }
     }
 }
